376d799a1d771e96d5f5f2febdf0df9ed3491662
tjhoe23 <125609687+tjhoe23@users.noreply.github.com>
2023-03-15 14:57:28 -0500

    finished exercise angular 2

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -23,8 +23,11 @@ export class CandidatesComponent implements OnInit {
   ngOnInit() {
   }
 
-  // Code the addToCrew function here:
-
+addToCrew(person: object) {
+  if (!this.crew.includes(person)) {
+    this.crew.push(person);
+  }
+}
 
   // BONUS: Code the changeMissionName function here:
 
